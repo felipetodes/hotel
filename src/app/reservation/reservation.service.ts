73c9805e7e998ee3ea6caf47690119b9fba38159
Felipe Todeschini <todeschini1992@gmail.com>
2025-02-13 21:45:10 -0300

    Atualização hotel10

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -10,5 +10,21 @@ export class ReservationService {
   getReservations(): Reservation[] {
     return this.reservations;
   }
+  getReservation(id: string): Reservation | undefined
+  {
+  return this.reservations.find(res => res.id === id);
+  }
+addReservation(reservation: Reservation): void {
+this.reservations.push(reservation);
+}
 
+deleReservation(id: string): void {
+let index = this.reservations.findIndex(res => res.id !== id);
+this.reservations.splice(index, 1);
+  }
+
+  updateReservation(updatedreservation: Reservation): void {
+    let index = this.reservations.findIndex(res => res.id === updatedreservation.id);
+    this.reservations.splice(index,1)
+  }
 }
