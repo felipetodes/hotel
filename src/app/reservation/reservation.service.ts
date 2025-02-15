4bceea2637f5faaa683a1e47ab7958c47e40d2e3
Felipe Todeschini <todeschini1992@gmail.com>
2025-02-14 23:58:09 -0300

    Atualização hotel13

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -16,6 +16,7 @@ export class ReservationService {
   }
 addReservation(reservation: Reservation): void {
 this.reservations.push(reservation);
+console.log(this.reservations);
 }
 
 deleReservation(id: string): void {
