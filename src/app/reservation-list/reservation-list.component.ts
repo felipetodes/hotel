c21616f6969db8ebc77dd1eca1ad97c48afb79a0
Felipe Todeschini <todeschini1992@gmail.com>
2025-02-15 19:14:53 -0300

    Atualização hotel15

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
 @Component({
   selector: 'app-reservation-list',
   standalone: false,
   templateUrl: './reservation-list.component.html',
   styleUrl: './reservation-list.component.css'
 })
-export class ReservationListComponent {
+export class ReservationListComponent implements OnInit {
+  reservartions: Reservation[] = [];
+
+  constructor(private reservationService: ReservationService){}
+
+  ngOnInit(): void {}
+
 
 }
