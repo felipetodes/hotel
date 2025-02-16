3eb59f9eb29faeba4bc5908d4fb4386ee7997925
Felipe Todeschini <todeschini1992@gmail.com>
2025-02-16 17:49:02 -0300

    Atualização hotel21

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -15,6 +15,9 @@ export class ReservationListComponent implements OnInit {
   ngOnInit(): void {
     this.reservartions = this.reservationService.getReservations();
   }
+deleteReservation(id: string) {
+  this.reservationService.deleReservation(id);
 
+}
 
 }
