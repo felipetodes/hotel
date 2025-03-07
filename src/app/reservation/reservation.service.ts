70a09901556229099db63832a006c4a4fd5f8dc2
Felipe Todeschini <todeschini1992@gmail.com>
2025-03-06 22:20:32 -0300

    Atualização1

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -6,11 +6,6 @@ import { Reservation } from '../models/reservation';
 export class ReservationService {
   private reservations: Reservation[] = [];
 
-  constructor() {
-    let savedReservations = localStorage.getItem('reservations');
-    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
-  }
-
   // CRUD
   getReservations(): Reservation[] {
     return this.reservations;
@@ -22,18 +17,15 @@ export class ReservationService {
 addReservation(reservation: Reservation): void {
 reservation.id = Date.now().toString();
 this.reservations.push(reservation);
-localStorage.setItem('reservations', JSON.stringify(this.reservations));
 }
 
 deleReservation(id: string): void {
 let index = this.reservations.findIndex(res => res.id !== id);
 this.reservations.splice(index, 1);
-localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 
   updateReservation(id: string, updatedReservation: Reservation): void {
     let index = this.reservations.findIndex(res => res.id === id);
     this.reservations[index] = updatedReservation;
-    localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 }
